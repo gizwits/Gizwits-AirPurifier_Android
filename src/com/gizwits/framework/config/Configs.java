eff1fe2d974e161f4e7950dd5cdda260a3372cd5
Pomia <pomia@gizwits.com>
2016-06-05 17:40:44 +0800

    update appid

--- src/com/gizwits/framework/config/Configs.java
+++ src/com/gizwits/framework/config/Configs.java
@@ -34,9 +34,9 @@ public class Configs {
 	public static final boolean DEBUG = true;
 
 	// 设定AppID，参数为机智云官网中查看产品信息得到的AppID
-	public static final String APPID = "31ee082710384a71bec246e1c9695920";
+	public static final String APPID = "your_app_id";
 	// 指定该app对应设备的product_key，如果设定了过滤，会过滤出该peoduct_key对应的设备
-	public static final String PRODUCT_KEY = "b3a206e380bc4a3c8c157ff4b5491633";
+	public static final String PRODUCT_KEY = "your_product_key";
 	// 设定日志打印级别
 
 	public static final XPGWifiLogLevel LOG_LEVEL = XPGWifiLogLevel.XPGWifiLogLevelAll;
@@ -47,6 +47,6 @@ public class Configs {
 	public static final String LOCATIONAK="F1e62d8e200e8aebda7c57a1633ae453";
 	
 	/** 产品密钥 */
-	public static final String APP_SECRET = "859fd2b5fef64a049f4bc8f7695abe71";
+	public static final String APP_SECRET = "your_app_secret";
 
 }
